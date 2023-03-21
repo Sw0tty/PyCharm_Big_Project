06c302f57c554a640b76a093ba9f14410347f707
Swotty <sw0tty@yandex.ru>
2023-03-21 16:37:55 +0300

    Mod Codewars

--- Codewars.py
+++ Codewars.py
@@ -1,6 +1,32 @@
-def binary_array_to_number(arr):
-    return int("".join(map(str, arr)), 2)
+def mispelled(word1, word2):
+    if abs(len(word1) - len(word2)) >= 2:
+        return False
+    else:
+        word1 = list(sorted(word1))
+        word2 = list(sorted(word2))
+        first = word1
+        second = word2
+        if len(word1) > len(word2):
+            first, second = word2, word1
+        for i in first:
+            if i in second:
+                second.remove(i)
+        if len(second) >= 2:
+            return False
+        else:
+            return True
 
+
+print(mispelled(word1="67qu", word2="67qu"))
+
+# Функция перевода из двоичной системы в 10-чную. На вход принимает список чисел
+def binary_array_to_number(arr):
+    return int("".join(map(str, arr)), 2)  # Число 2 означает, что на вход будут приниматься лишь двочные числа
+
+
+print(binary_array_to_number([1, 0, 1]))
+
+# ----------------
 
 def add_binary(a=2,b=2):
     return (f"""{a} + {b} = {a + b} in decimal or {format(a + b, "b")} in bianry""")

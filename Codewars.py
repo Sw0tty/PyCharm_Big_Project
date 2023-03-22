3decc9e789a53aeffff959a1124470ad0603e8f3
Swotty <sw0tty@yandex.ru>
2023-03-22 15:20:19 +0300

    Codewars

--- Codewars.py
+++ Codewars.py
@@ -1,3 +1,39 @@
+def move_zeros(lst):
+    count = 0
+    for i in lst:
+        if i == 0:
+            count += 1
+    while count:
+        lst.remove(0)
+        lst.append(0)
+        count -= 1
+    return lst
+
+print(move_zeros([1, 0, 1, 2, 0, 1, 3]))
+
+# def move_zeros(a):
+#     a.sort(key=lambda v: v == 0)
+#     return a
+
+
+
+
+def speed_limit(speed, signals):
+    fine = 0
+    for i in signals:
+        print(speed, i)
+        if speed > i:
+            if 10 <= abs(i - speed) <= 19:
+                fine += 100
+            elif 20 <= abs(i - speed) <= 29:
+                fine += 250
+            elif abs(i - speed) >= 30:
+                fine += 500
+        print(fine)
+    return fine
+
+print(speed_limit(speed=130, signals=[140, 130, 100]))
+print()
 def mispelled(word1, word2):
     if abs(len(word1) - len(word2)) >= 2:
         return False
